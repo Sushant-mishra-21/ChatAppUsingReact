9221bf8e1f592cad4586aabaf1ec286332013cc5
Sushant-mishra-21 <sushant.mishra@globallogic.com>
2023-04-22 07:55:43 +0530

    Initalized the Chatting environment using react-chat-engine

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
-import { ChatEngine } from 'react-chat-engine';
-import './App.css';
-
-function App() {
+import { ChatEngine } from "react-chat-engine";
+import "./App.css";
+import ChatFeed from "./components/ChatFeed.jsx";
+function App(){
   return (
-   <>
-   </>
+    <>
+      <ChatEngine
+        height="120vh"
+        projectID="
+        8a2a2f20-6dc3-4e99-89ad-c4162f251bbb"
+        userName="John"
+        userSecret="12345"
+        renderChatFeed={(chatAppProps)=><ChatFeed  {...chatAppProps}/>}
+        onNewMessage={()=> new Audio('https://chat-engine-assets.s3.amazonaws.com/click.mp3').play}/>
+    </>
   );
 }
 
